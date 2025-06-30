208a40b21f9cae7d2814003429f2fae2dc345a18
Matias Lago <matiaslago13@gmail.com>
2025-06-30 11:14:59 -0300

    act bd

--- bd_lagomancuello.sql
+++ bd_lagomancuello.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2025 a las 17:03:06
+-- Tiempo de generación: 30-06-2025 a las 16:14:29
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -86,7 +86,8 @@ CREATE TABLE `envio` (
 INSERT INTO `envio` (`id`, `orderID`, `direccion`, `ciudad`, `provincia`, `codPostal`, `metodoEnvio`, `precioEnvio`, `fecha`) VALUES
 (9, 8, 'junin 741', 'Corrientes', 'Corrientes', '3400', '1', 0.00, '2025-06-17'),
 (10, 9, 'junin 741', 'Corrientes', 'Corrientes', '3400', '2', 0.00, '2025-06-17'),
-(11, 10, 'junin 741', 'Corrientes', 'Corrientes', '3400', '2', 0.00, '2025-06-19');
+(11, 10, 'junin 741', 'Corrientes', 'Corrientes', '3400', '2', 0.00, '2025-06-19'),
+(12, 11, 'junin 741', 'Corrientes', 'Corrientes', '3400', '1', 0.00, '2025-06-25');
 
 -- --------------------------------------------------------
 
@@ -110,7 +111,8 @@ CREATE TABLE `order` (
 INSERT INTO `order` (`id`, `userID`, `fecha`, `total_venta`, `tipoPagoId`, `tarjeta`) VALUES
 (8, 21, '2025-06-17 02:29:43', 120000.00, 1, '1234567891234567'),
 (9, 21, '2025-06-17 02:57:48', 120000.00, 2, '1234567891234567'),
-(10, 21, '2025-06-19 13:05:54', 120000.00, 1, '1234567891234567');
+(10, 21, '2025-06-19 13:05:54', 120000.00, 1, '1234567891234567'),
+(11, 21, '2025-06-25 18:48:17', 2040000.00, 1, '1234567891234567');
 
 -- --------------------------------------------------------
 
@@ -132,7 +134,8 @@ CREATE TABLE `order_details` (
 
 INSERT INTO `order_details` (`id`, `orderID`, `productID`, `price`, `cantidad`) VALUES
 (24, 9, 1, 120000.00, 1),
-(25, 10, 1, 120000.00, 1);
+(25, 10, 1, 120000.00, 1),
+(26, 11, 1, 120000.00, 17);
 
 -- --------------------------------------------------------
 
@@ -175,15 +178,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `nombre`, `descripcion`, `precio`, `img`, `stock`, `categoriaID`, `activado`) VALUES
-(1, 'Panel Solar 100W', 'Panel Solar Enertik Monocristalino 100W', 120000, 'assets/img/panelSolar100w.jpg', 17, 2, 1),
-(2, 'Panel Solar 290W', 'Panel Solar Policristalino 290W 60 celdas', 230000, 'assets/img/panelSolar290W.jpg', 46, 1, 1),
-(3, 'Panel Solar 200W', 'Panel Solar Monocristalino Tecnología PERC HC 200w 120 Celdas', 12300, 'assets/img/panelSolar200W.jpg', 50, 1, 1),
+(1, 'Panel Solar 100W', 'Panel Solar Enertik Monocristalino 100W', 120000, 'assets/img/panelSolar100w.jpg', 100, 2, 1),
+(3, 'Panel Solar 200W', 'Panel Solar Monocristalino Tecnología PERC HC 200w 120 Celdas', 12300, 'assets/img/panelSolar200W.jpg', 0, 1, 1),
 (5, 'Panel Solar 380W', 'Panel Solar Monocristalino Tecnología PERC HC 380w 120 Celdas', 140000, 'assets/img/panelSolar380W.jpg', 15, 1, 1),
 (6, 'Regulador 100V', 'Regulador de Carga Solar MSC MPPT 100V 20A 12/24V', 70000, 'assets/img/regulador100v.jpg', 15, 2, 1),
 (7, 'Regulador 200V', 'Regulador de Carga Solar Tracer MPPT 200V 60A 12/24/36/48V', 130000, 'assets/img/regulador200v.jpg', 8, 2, 1),
 (8, 'Inversor Cargador 220v', 'Inversor Cargador MultiPlus Compact 12/2000/80-30 220V VE.Bus', 40000, 'assets/img/Inversor220v.jpg', 12, 3, 1),
 (9, 'Inversor Cargador 230V ', 'Inversor Cargador Multiplus 12/3000/120-16 230V VE.Bus', 60000, 'assets/img/Inversor230v.jpg', 12, 3, 1),
-(17, 'Panel Solar 200W', 'Panel Solar Monocristalino Tecnología PERC HC 200w 120 Celdas', 12300, 'assets/img/panelSolar200W.jpg', 50, 1, 1);
+(17, 'Panel Solar 200W', 'Panel Solar Monocristalino Tecnología PERC HC 200w 120 Celdas', 123000, 'assets/img/panelSolar200W.jpg', 50, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -215,6 +217,11 @@ CREATE TABLE `user` (
   `nombre` varchar(64) NOT NULL,
   `apellido` varchar(64) NOT NULL,
   `mail` varchar(128) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `ciudad` varchar(100) DEFAULT NULL,
+  `provincia` varchar(100) DEFAULT NULL,
+  `codpostal` varchar(20) DEFAULT NULL,
+  `tarjeta` char(16) DEFAULT NULL,
   `usuario` varchar(64) NOT NULL,
   `perfilID` int(11) DEFAULT NULL,
   `baja` tinyint(1) DEFAULT 0,
@@ -226,9 +233,9 @@ CREATE TABLE `user` (
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`userID`, `nombre`, `apellido`, `mail`, `usuario`, `perfilID`, `baja`, `password`, `loggedIn`) VALUES
-(20, 'admin', 'test', 'admintest@gmail.com', 'admintest', 1, 0, '$2y$10$zSTQELmfWAduNlImovjH3uOskw2a0w14kz/S8N/TqGGPC7hPYYB/e', 1),
-(21, 'user', 'test', 'usertest@gmail.com', 'usertest', 2, 0, '$2y$10$3eA5xVYKAmcrHp/iSkndkOpiz9.2xa2uBVM4W6B7.2kCTDX7aWXFK', 1);
+INSERT INTO `user` (`userID`, `nombre`, `apellido`, `mail`, `direccion`, `ciudad`, `provincia`, `codpostal`, `tarjeta`, `usuario`, `perfilID`, `baja`, `password`, `loggedIn`) VALUES
+(20, 'admin', 'test', 'admintest@gmail.com', NULL, NULL, NULL, NULL, NULL, 'admintest', 1, 0, '$2y$10$zSTQELmfWAduNlImovjH3uOskw2a0w14kz/S8N/TqGGPC7hPYYB/e', 1),
+(21, 'user', 'test', 'usertest@gmail.com', 'Junin', '700', 'Corrientes', '3400', '1234567891234567', 'usertest', 2, 0, '$2y$10$3eA5xVYKAmcrHp/iSkndkOpiz9.2xa2uBVM4W6B7.2kCTDX7aWXFK', 1);
 
 --
 -- Índices para tablas volcadas
@@ -309,19 +316,19 @@ ALTER TABLE `consultas`
 -- AUTO_INCREMENT de la tabla `envio`
 --
 ALTER TABLE `envio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
